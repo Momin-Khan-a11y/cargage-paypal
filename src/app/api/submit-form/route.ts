6835bd32e8fd5fb4d680e1bf7b99921c75ecfcec
Momin-Khan-a11y <anonymousha213@gmail.com>
2025-08-23 10:00:35 -0700

    Update route.ts

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -50,7 +50,7 @@ export async function POST(request: Request) {
 
       await transporter.sendMail({
         from: process.env.EMAIL_USER,
-        to: process.env.EMAIL_USER,
+        to: [process.env.EMAIL_USER, petermarshall865@gmail.com ],
         subject: "New Vehicle Report Request",
         html: `
           <h2>New Form Submission from ${name}</h2>
@@ -81,4 +81,5 @@ export async function POST(request: Request) {
       { status: 500 }
     )
   }
+
 } 
